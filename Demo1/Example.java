e6de7bf5e2599894ccebdfcd0416fbcff2843c66
Punsan2546 <654259026@webmail.npru.ac.th>
2022-12-16 11:41:11 +0700

    Edit - first commit

--- Demo1/Example.java
+++ Demo1/Example.java
@@ -1,5 +1,9 @@
 public class Example{
     public static void main (string[] args){
         system.out.println("Hello world");
+        myMethod();
+    }
+    public void myMethod () {
+        system.out.println("Punsan Somkla")
     }
 }
