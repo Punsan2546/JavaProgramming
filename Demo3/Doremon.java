9cba35513a5f4b8897d91069b9412d6e7cab77f0
Punsan2546 <654259026@webmail.npru.ac.th>
2023-01-22 15:20:44 +0700

    Update 3:20

--- Demo3/Doremon.java
+++ Demo3/Doremon.java
@@ -1,18 +1,29 @@
+
 import javax.swing.JOptionPane;
 public class Doremon {
     public static void main(String[] args) {
-        String data = JOptionPane.showInputDialog(null, "Hi! What is your name?");
-        JOptionPane.showMessageDialog(null, "Hello,My name is:  " + data);
+        String data = JOptionPane.showInputDialog(null, "Hi! What is your name?", "Doremon",JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, "Hello,My name is:  " + data , "Me",JOptionPane.INFORMATION_MESSAGE);
 
-        JOptionPane.showMessageDialog(null, "Oh! hi , :  " + data);
-        JOptionPane.showMessageDialog(null, "What can do you for you?  ");
-        JOptionPane.showMessageDialog(null, "I want to go to the future.  ");
+        JOptionPane.showMessageDialog(null, "Oh! hi , :  " + data, "Doremon",JOptionPane.INFORMATION_MESSAGE );
+        JOptionPane.showMessageDialog(null, "What can do you for you?  ", "Doremon",JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, "I want to go to the future.", "Xinan", JOptionPane.INFORMATION_MESSAGE);
 
-        int intt = JOptionPane.showInputDialog(null, "Of course, What is the present year? ");
-        JOptionPane.showMessageDialog("It is : " + intt);
+        // STRING intt = JOptionPane.showInputDialog(null, "Of course, What is the present year? ");
+        int intt = Integer.parseInt(JOptionPane.showInputDialog(null, "Of course, What is the present year? ", "Doremon",JOptionPane.INFORMATION_MESSAGE));
+        JOptionPane.showConfirmDialog(null, "It is : "+ intt, "Xinan",JOptionPane.INFORMATION_MESSAGE);
+        
 
-        JOptionPane.showMessageDialog(null,"Ok. How many years do you want to travel? "title.Doremon);
+        int nextyear = Integer.parseInt(JOptionPane.showInputDialog(null,"Ok. How many years do you want to travel?", "Doremon",JOptionPane.INFORMATION_MESSAGE));
+        JOptionPane.showMessageDialog(null, "I want to go for : "+ nextyear, "Xinan",JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, "Ok. Lets Gooooooooooo!", "Doremon",JOptionPane.INFORMATION_MESSAGE);
+        
+        JOptionPane.showMessageDialog(null, ".............................", "tamacine",JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, ".............................", "tamacine",JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, ".............................", "tamacine",JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, ".............................", "tamacine",JOptionPane.INFORMATION_MESSAGE);
 
-        JOptionPane.showMessageDialog(null, )
-    }
+        JOptionPane.showConfirmDialog(null, "Hello Welcome to "+ (intt+nextyear) , "Doremon",JOptionPane.INFORMATION_MESSAGE );
+
+}
 }
