229678a9ee7876f477f47292bfe7af31a5c8ada6
Punsan2546 <654259026@webmail.npru.ac.th>
2023-01-27 12:17:46 +0700

    Update 12:17

---
+++ java/Exercise5.java
@@ -0,0 +1,22 @@
+import java.utill.Scanner;
+
+public class Exercise5{
+    public main void main (String[] args) {
+        Scanner scan = new Scanner (System.in);
+        System.out.println("ENter your weight (KG) ");
+        double weight = scan.nextDouble();
+        System.out.println("ENter your Height (CM) ");
+        double height = scan.nextDouble();
+        double h = (height/100);
+        double bmi = weight /(h*h);
+        System.out.println("Your bmi is : " + bmi);
+        String status = " ";
+        if (bmi >= 30)
+        status = "Very fat";
+        else if (bmi>=25)
+        status = "Fat";
+        else if (bmi>=23)
+        status = "";
+        else if (bmi)
+    }
+}
